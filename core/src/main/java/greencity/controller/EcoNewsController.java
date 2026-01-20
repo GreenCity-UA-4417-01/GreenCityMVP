b6f34a18d39efeee5b8b2f4e793e5d61f0aff8e4
sandiukp <sandiukp@gmail.com>
2026-01-20 14:11:36 +0200

    fix(econews): document 401 Unauthorized for PUT /econews/update

--- core/src/main/java/greencity/controller/EcoNewsController.java
+++ core/src/main/java/greencity/controller/EcoNewsController.java
@@ -149,6 +149,7 @@ public class EcoNewsController {
                     content = @Content(schema = @Schema(implementation = EcoNewsGenericDto.class))),
             @ApiResponse(responseCode = "303", description = HttpStatuses.SEE_OTHER),
             @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
             @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN),
             @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
     })
