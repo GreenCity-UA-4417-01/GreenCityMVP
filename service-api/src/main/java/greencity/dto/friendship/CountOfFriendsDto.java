e24834091465a64cc28c82efd081120366c91600
AdamT3H <adamogorodnik@gmail.com>
2026-02-02 01:34:42 +0200

    Added controller, servide and dto

---
+++ service-api/src/main/java/greencity/dto/friendship/CountOfFriendsDto.java
@@ -0,0 +1,12 @@
+package greencity.dto.friendship;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CountOfFriendsDto {
+    private Long friendshipCount;
+}
